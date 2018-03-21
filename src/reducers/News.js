c1649d5c4e35acba951147ef6df9308f1660f02f
archit8 <singh.architsinghaim@gmail.com>
2018-03-21 14:23:07 +0530

    Update News.js

--- src/reducers/News.js
+++ src/reducers/News.js
@@ -1,21 +1,12 @@
-import {FETCH_NEWS_SUCCESS, FETCH_NEWS_ERROR,FETCH_TOP_NEWS_SUCCESS,FETCH_SPORTS_NEWS_SUCCESS,FETCH_WORLD_NEWS_SUCCESS} from '../constants/ActionTypes';
+import {FETCH_NEWS_CATEGORY_SUCCESS, FETCH_NEWS_ERROR,FETCH_TOP_NEWS_SUCCESS,FETCH_SPORTS_NEWS_SUCCESS,FETCH_WORLD_NEWS_SUCCESS} from '../constants/ActionTypes';
 
 export default function news(state = [], action) {
 	let newState = [...state];
 	switch (action.type) {
-		case FETCH_NEWS_SUCCESS:
+		case FETCH_NEWS_CATEGORY_SUCCESS:
 			newState = [...action.news];
 			break;
 		case FETCH_NEWS_ERROR:
-			break;
-		case FETCH_TOP_NEWS_SUCCESS:
-			newState = [...action.news];
-			break;
-		case FETCH_SPORTS_NEWS_SUCCESS:
-			newState = [...action.news];
-			break;
-		case FETCH_WORLD_NEWS_SUCCESS:
-			newState = [...action.news];
 			break;
 		default: 
 			break;
